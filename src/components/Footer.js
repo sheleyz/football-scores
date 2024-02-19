f564b200841c1bd4c0c4c57a00ab14eb361effd4
sheleyz <59847602+sheleyz@users.noreply.github.com>
2024-02-19 11:20:59 -0500

    Loaded updated game data JSON file, updated team data JSON file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ export default function Footer() {
         <footer className="bg-brand-secondary">
             <div className="footerText container d-flex flex-column align-items-center px-4 px-lg-5 pt-4 fw-semibold">
                 <div className="text-center pt-4 pb-3 fw-normal text-white">
-                    <p className="mb-0"><i>Game data updated through 1/28/2024.</i></p>
-                    <p><i>Team data updated through 1/28/2024.</i></p>
+                    <p className="mb-0"><i>Game data updated through 2/11/2024.</i></p>
+                    <p><i>Team data updated through 2/11/2024.</i></p>
                     <p>Created by <a href="https://zachsheley.com/" target="_blank" rel="noopener noreferrer">Zach Sheley</a> - Powered by <a href="https://www.kaggle.com/datasets/tobycrabtree/nfl-scores-and-betting-data" target="_blank" rel="noopener noreferrer">this dataset</a> and <a href="https://corndoggydrawings.com/football-doodles" target="_blank" rel="noopener noreferrer">these team logo drawings</a>.</p>
                     <p className="text-start">Disclaimer: This website is not affiliated, associated, authorized, endorsed by, or in any way officially connected with the National Football League (NFL), or any of its subsidiaries or its affiliates. NFL is a registered trademark of the National Football League. The team names and logos are registered trademarks of the teams indicated. All other NFL-related trademarks are trademarks of the National Football League.</p>
                 </div>
