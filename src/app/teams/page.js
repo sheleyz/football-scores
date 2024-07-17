ef800fde7d9d497d1828460c8253596726cc0979
sheleyz <59847602+sheleyz@users.noreply.github.com>
2024-07-16 21:27:29 -0400

    Added page metadata

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -15,3 +15,40 @@ export default function TeamsPage() {
         </Layout>
     );
 }
+
+export const metadata = {
+    title: "Teams | Football Scores",
+    description: "Football Scores provides final scores, betting odds (when available), and other details for all NFL games since 1966.",
+    image: "./icon.png",
+    metadataBase: new URL("https://nflscores.vercel.app"),
+
+    openGraph: {
+        title: "Teams | Football Scores",
+        description: "Football Scores provides final scores, betting odds (when available), and other details for all NFL games since 1966.",
+        url: "https://nflscores.vercel.app",
+        siteName: "Football Scores",
+        images: ["./icon.png"],
+        locale: "en_US",
+        type: "website"
+    },
+
+    twitter: {
+        card: "summary",
+        title: "Teams | Football Scores",
+        description: "Football Scores provides final scores, betting odds (when available), and other details for all NFL games since 1966.",
+        images: ["./icon.png"],
+        url: "https://nflscores.vercel.app"
+    },
+
+    icons: {
+        icon: "./icon.png",
+        shortcut: "./icon.png"
+    },
+
+    verification: {
+        google: "mUwMofjCd9iaaQC1nkCZaoJrxfv95Ais8OMQTxqae0A",
+        other: {
+            "msvalidate.01": "913751F24A1482E2603AE560D9749A01"
+        }
+    }
+};
