fb2ca95b56d8fdce37ac8e7f8f2e4de286dd4e3d
sheleyz <59847602+sheleyz@users.noreply.github.com>
2024-08-06 11:44:36 -0400

    Added robots and sitemap files

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://nflscores.vercel.app/",
+            lastModified: new Date()
+        },
+        {
+            url: "https://nflscores.vercel.app/teams",
+            lastModified: new Date()
+        }
+    ];
+}
