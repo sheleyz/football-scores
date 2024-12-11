d64e9d91ea8b196e5aa67f3f00fc2948d6ffd783
sheleyz <59847602+sheleyz@users.noreply.github.com>
2024-12-10 19:18:46 -0500

    Loaded updated game data JSON file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="bg-brand-secondary">
             <div className="footerText container d-flex flex-column align-items-center px-3 px-lg-5 pt-4 fw-semibold">
                 <div className="text-center pt-4 pb-3 fw-normal text-white">
-                    <p className="mb-0"><i>Game data updated through 11/25/2024.</i></p>
+                    <p className="mb-0"><i>Game data updated through 12/2/2024.</i></p>
                     <p><i>Team data updated through 12/2/2024.</i></p>
                     <p>Created by <a href="https://zachsheley.com/" target="_blank" rel="noopener noreferrer">Zach Sheley</a> - Powered by <a href="https://www.kaggle.com/datasets/tobycrabtree/nfl-scores-and-betting-data" target="_blank" rel="noopener noreferrer">this dataset</a> and <a href="https://corndoggydrawings.com/football-doodles" target="_blank" rel="noopener noreferrer">these team logo drawings</a>.</p>
                     <p><a href="https://form.jotform.com/240006603463142" target="_blank" rel="noopener noreferrer">Let me know</a> if you have any suggestions to improve this site.</p>
