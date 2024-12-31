902544339db23bd9173f804570b6a3d6abb774a5
sheleyz <59847602+sheleyz@users.noreply.github.com>
2024-12-31 13:38:13 -0500

    Use Next library for GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 import localFont from "next/font/local";
 import { Providers } from "./providers";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const outfit = localFont({
     src: "./fonts/Outfit/Outfit-VariableFont_wght.ttf",
@@ -55,16 +55,7 @@ export default function RootLayout({ children }) {
             <body className={outfit.variable}>
                 <Providers>{children}</Providers>
             </body>
-
-            {/* Google tag (gtag.js) */}
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H9K5XVBHBF"></Script>
-            <Script id="google-analytics">
-                {`window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-H9K5XVBHBF');`}
-            </Script>
+            <GoogleAnalytics gaId="G-H9K5XVBHBF" />
         </html>
     );
 }
